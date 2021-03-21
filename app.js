561ae00ab9d57c8b61cc2ba52d7d70cbdbb47059
Ata-92 <atakal92@gmail.com>
2021-03-21 16:56:13 +0300

    Use dom manipulation rather than write method

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const text = prompt('Please enter a text: ');
 let reversedText = '';
 for (let i = text.length - 1; i >= 0; i--) reversedText += text[i];
-document.write(reversedText);
+document.getElementById("p").innerHTML = reversedText;
